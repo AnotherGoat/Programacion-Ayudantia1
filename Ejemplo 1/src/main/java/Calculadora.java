7ea9818121ee429e15cbb0cf37043306b4c22631
VictorMardones <61598238+VictorMardones@users.noreply.github.com>
2020-05-17 13:58:54 -0400

    Comenzar ejemplo calculadora

---
+++ Ejemplo 1/src/main/java/Calculadora.java
@@ -0,0 +1,44 @@
+public class Calculadora {
+
+    // Atributos
+    private int num1, num2;
+
+    // Constructor
+    public Calculadora(int num1, int num2) {
+        this.num1 = num1;
+        this.num2 = num2;
+    }
+
+    public int sumar(){
+        return num1+num2;
+    }
+
+    public int restar(){
+        return num1-num2;
+    }
+
+    public int multiplicar(){
+        return num1*num2;
+    }
+
+    public double dividir(){
+        return (double)num1/num2;
+    }
+
+    // Getters y Setters
+    public int getNum1() {
+        return num1;
+    }
+
+    public void setNum1(int num1) {
+        this.num1 = num1;
+    }
+
+    public int getNum2() {
+        return num2;
+    }
+
+    public void setNum2(int num2) {
+        this.num2 = num2;
+    }
+}
