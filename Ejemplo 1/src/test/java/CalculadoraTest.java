f351a2c75da10bbadf74039f43e459042559f6af
VictorMardones <61598238+VictorMardones@users.noreply.github.com>
2020-05-17 14:40:37 -0400

    Añadido .gitignore

--- Ejemplo 1/src/test/java/CalculadoraTest.java
+++ Ejemplo 1/src/test/java/CalculadoraTest.java
@@ -6,33 +6,47 @@ import static org.junit.Assert.*;
 
 public class CalculadoraTest {
 
-    Calculadora c;
+    Calculadora a, c;
 
-    @Before
+    @Before // Antes de hacer todos los test
     public void setUp() throws Exception {
+        a = new Calculadora(6, 2);
+        c = new Calculadora(5, 1);
     }
 
-    @After
+    @After // Después de hacer todos los test
     public void tearDown() throws Exception {
+        a = null; // Liberar memoria
+        c = null;
     }
 
     @Test
     public void sumar() {
+        assertEquals(8, a.sumar());
+        assertEquals(6, c.sumar());
     }
 
     @Test
     public void restar() {
+        assertEquals(4, a.restar());
+        assertEquals(4, c.restar());
     }
 
     @Test
     public void multiplicar() {
+        assertEquals(12, a.multiplicar());
+        assertEquals(5, c.multiplicar());
     }
 
     @Test
     public void dividir() {
+        assertEquals(3, a.dividir(), 0);
+        assertEquals(5, c.dividir(), 0);
     }
 
     @Test
     public void potencia() {
+        assertEquals(36, a.potencia(), 0);
+        assertEquals(5, c.potencia(), 0);
     }
 }
